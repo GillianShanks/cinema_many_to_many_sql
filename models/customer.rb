b5ed2801888561a72a32e3c5e5cbf0885fb31888
Gillian Shanks <GillianShanks88@gmail.com>
2019-08-02 15:40:43 +0100

    adds films() function to get all the films for a customer, tested in pry

--- models/customer.rb
+++ models/customer.rb
@@ -1,4 +1,5 @@
 require_relative('../db/sql_runner')
+require_relative('./film.rb')
 
 class Customer
 
@@ -43,5 +44,12 @@ class Customer
     values = [@id]
     SqlRunner.run(sql, values)
   end
-  
+
+  def films()
+    sql = "SELECT films.* FROM films INNER JOIN tickets ON films.id = tickets.film_id WHERE tickets.customer_id = $1"
+    values = [@id]
+    films = SqlRunner.run(sql,values)
+    return films.map{|film|Film.new(film)}
+  end
+
 end
