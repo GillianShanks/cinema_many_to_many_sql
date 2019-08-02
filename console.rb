4c666ba5bdd7f71d46c33c39b05633e5baa9c36b
Gillian Shanks <GillianShanks88@gmail.com>
2019-08-02 13:36:53 +0100

    Initial commit - folders and new films created in console.rb

---
+++ console.rb
@@ -0,0 +1,28 @@
+require('pg')
+require_relative('./models/ticket.rb')
+require_relative('./models/film.rb')
+require_relative('./models/customer.rb')
+
+film1 = Film.new(
+  {
+    'title' => 'Happy Unicorns',
+    'price' => 5
+  }
+)
+
+film2 = Film.new(
+  {
+    'title' => 'Big Disaster',
+    'price' => 10
+  }
+)
+
+film3 = Film.new(
+  {
+    'title' => 'Totally In Love',
+    'price' => 6
+  }
+)
+
+binding.pry
+nil
