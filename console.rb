b4a88545f0b3978bab6518ec48bb568b50e604ed
Gillian Shanks <GillianShanks88@gmail.com>
2019-08-02 13:44:11 +0100

    Adds Film class

--- console.rb
+++ console.rb
@@ -1,4 +1,4 @@
-require('pg')
+require('pry-byebug')
 require_relative('./models/ticket.rb')
 require_relative('./models/film.rb')
 require_relative('./models/customer.rb')
---
+++ models/film.rb
@@ -0,0 +1,14 @@
+require_relative('../db/sql_runner.rb')
+
+class Film
+
+  attr_reader :id
+  attr_accessor :title, :price
+
+  def initialize(film)
+    @id = film['id'].to_i if film['id']
+    @title = film['title']
+    @price = film['price'].to_i
+  end
+
+end
