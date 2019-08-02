5beadaa5ea2564f552c6b7f3923b2278b9243ff3
Gillian Shanks <GillianShanks88@gmail.com>
2019-08-02 15:21:01 +0100

    adds uppdate() function for a ticket, tested in pry

--- models/ticket.rb
+++ models/ticket.rb
@@ -1,6 +1,9 @@
 require_relative('../db/sql_runner.rb')
 
 class Ticket
+  attr_reader :id
+  attr_accessor :film_id, :customer_id
+  
   def initialize(ticket)
     @id = ticket['id'].to_i if ticket['id']
     @film_id = ticket['film_id'].to_i
@@ -17,6 +20,12 @@ class Ticket
   def self.delete_all()
     sql = "DELETE FROM tickets"
     SqlRunner.run(sql)
+  end
+
+  def update()
+    sql = "UPDATE tickets SET (film_id, customer_id) = ($1,$2) WHERE id = $3"
+    values = [@film_id, @customer_id, @id]
+    SqlRunner.run(sql, values)
   end
 
 
